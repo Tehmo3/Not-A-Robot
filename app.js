1ef750db1cec118a8ca06a86d4cb1f68301ce724
Tehmo3 <jasper.b.miles@gmail.com>
2017-03-01 11:54:52 +1100

    Almost working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var	Discord = require("discord.js"),
 	config = require("./config.json"),
-	fs = require("fs");
+	fs = require("fs"),
+	Chain = require('markov-chains').default;
 
 var client = new Discord.Client();
 
@@ -16,14 +17,40 @@ function output(error, token) {
 		console.log("Logged in!");
 	}
 }
-
+var userID;
 client.on('message', message => {
-  if (message.content === 'get chat logs') {
-    message.channel.fetchMessages({limit: 100})
-	.then(messages => logMessages(messages, message))
-	.catch(console.error)
+	var messageArray = message.content.split(" ");
+	if (messageArray[0] === '!log') {
+    	message.channel.fetchMessages({limit: 100})
+		.then(messages => logMessages(messages, message))
+		.catch(console.error)
 	}
+	else if (messageArray[0] === "!text") {
+		var obj = JSON.parse(fs.readFileSync('textLogs.json', 'utf8'));
+		var username = messageArray[1];
+		client.users.forEach(user => findID(username, user, userID, obj));
+	}
+
+
 })
+
+var findID = function(username, user, userID, obj) {
+	if(user.username === username) {
+		 userID = user.id;
+		 makeChain(userID, obj);
+		 return;
+	}
+}
+
+var makeChain = function(user, obj) {
+	if (user && obj) {
+		user = '<@' + user + '>'
+		const states = obj[user];
+		const chain = new Chain(states);
+		const sentence = chain.walk();
+		console.log(sentence);
+	}
+}
 
 var messageObject = {};
 var last;
@@ -55,7 +82,7 @@ var fetchMoreMessages = function(message, messageLast) {
 
 var insertMessages = function(message) {
 	if (messageObject[message.author]) {
-		messageObject[message.author].push({content: message.content, id: message.id})
+		messageObject[message.author].push([message.content])
 	}
 	else {
 		messageObject[message.author] = [];
