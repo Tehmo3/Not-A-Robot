211ab4a0c0693588d1038d5776d3f2749560f1b4
Tehmo3 <jasper.b.miles@gmail.com>
2017-07-19 22:44:11 +1000

    functionality

--- helpers/!advice.js
+++ helpers/!advice.js
@@ -5,9 +5,8 @@ const request = require('request');
 function sendAdvice(channel) {
   request('http://inspirobot.me/api?generate=true', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      botPayload.text = "@" + req.body.user_name + ": " + req.body.command + " " + body;
       // send Payload
-      channel.sendMessage(response)
+      channel.sendMessage(response.body)
       .then(message => console.log(`Sent message: ${message.content}`))
       .catch(console.error);
     }
