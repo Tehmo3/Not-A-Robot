197888cc6893f0fe66138b01ca20a4d50db6b255
Tehmo3 <jasper.b.miles@gmail.com>
2017-07-20 17:00:06 +1000

    typo

--- bot.js
+++ bot.js
@@ -78,7 +78,7 @@ client.on('message', message => {
           return;
         }
         else if(channel.channels.indexOf(message.channel.name) === -1) {
-          message.channel.sendMessage("```Sorry, the bot is in another channel right now!");
+          message.channel.sendMessage("```Sorry, the bot is in another channel right now!```");
           console.log("not a valid channel")
           return;
         }
