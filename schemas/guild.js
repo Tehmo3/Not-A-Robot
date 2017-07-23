3e6106ade9c8982213ea4eb10264c1fa7eb43af5
Tehmo3 <jasper.b.miles@gmail.com>
2017-07-23 19:31:43 +1000

    commit

---
+++ schemas/guild.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+module.exports = new Schema({
+  guildID: String,
+  allowedChannels: [String],
+  channels: [String],
+  blacklist: [String],
+  textQuiz: Schema.Types.Mixed,
+  linkQuiz: Schema.Types.Mixed,
+  refreshRate: Number,
+  lastRefresh: Schema.Types.Mixed
+});
