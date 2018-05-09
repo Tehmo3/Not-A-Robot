10cf18766c66c477e3154b0298429ae37fc92aff
Tehmo3 <jasper.b.miles@gmail.com>
2018-05-09 11:11:54 +1000

    cleaning

--- secrets.js
+++ secrets.js
@@ -1,4 +1,4 @@
 module.exports = {
-  appBotUserToken: 'NDQzMjI4NDQ5MDUwNzIyMzA0.DdKUPg.rpJIVR-Lp8ELPTsnTD36om8lUDA',
+  appBotUserToken: 'NDQzMjI4NDQ5MDUwNzIyMzA0.DdPb9w.h6c7e2yTGmClstNBok04vTVOxEE',
   mongodbURI: 'mongodb://localhost:27017'
 }
