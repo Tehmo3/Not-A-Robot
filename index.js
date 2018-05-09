77353a427cc22dac8cc45f3b53a7040079857aca
Tehmo3 <jasper.b.miles@gmail.com>
2018-05-09 11:10:59 +1000

    new token, hiding from repo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { CommandoClient } = require('discord.js-commando');
 const path = require('path');
 const SECRETS = require('./secrets.js');
 const mongoose = require('mongoose');
+const express = require('express');
 
 const client = new CommandoClient({
   commandPrefix: '$',
@@ -9,6 +10,21 @@ const client = new CommandoClient({
   disableEveryone: true
 })
 
+const app = express();
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', '/home.html'), null, (err) => {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent homepage');
+    }
+  });
+});
+app.listen(port);
 
 mongoose.connect(SECRETS.mongodbURI);
 
@@ -17,15 +33,15 @@ mongoose.connection.on('error', (err) => {
 });
 
 client.registry
-    .registerDefaultTypes()
-    .registerGroups([
-        ['setupcommands', 'A set of commands to handle the set up of the bot'],
-        ['impersonatecommands', 'A set of commands based on impersonating your friends'],
-        ['quizcommands', 'A set of commands for running and answering quizzes']
-    ])
-    .registerDefaultGroups()
-    .registerDefaultCommands()
-    .registerCommandsIn(path.join(__dirname, 'commands'));
+  .registerDefaultTypes()
+  .registerGroups([
+    ['setupcommands', 'A set of commands to handle the set up of the bot'],
+    ['impersonatecommands', 'A set of commands based on impersonating your friends'],
+    ['quizcommands', 'A set of commands for running and answering quizzes']
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands()
+  .registerCommandsIn(path.join(__dirname, 'commands'));
 
 
 client.on('ready', () => {
