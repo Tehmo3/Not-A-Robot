d567dc696eda26ab197fdfd0b3701b62b37e86f2
Tehmo3 <jasper.b.miles@gmail.com>
2018-05-09 12:05:50 +1000

    get tags

--- commands/funcommands/disguise.js
+++ commands/funcommands/disguise.js
@@ -1,5 +1,8 @@
 const { Command } = require('discord.js-commando');
 const request = require('request-promise-native');
+const vision = require('@google-cloud/vision');
+const fs = require('fs');
+// const path = require('path');
 
 module.exports = class Advice extends Command {
     constructor(client) {
@@ -14,7 +17,7 @@ module.exports = class Advice extends Command {
                 key: 'image',
                 prompt: 'What should your profile picture be?',
                 type: 'string',
-                default: null //If null, use a pre-determined image
+                default: '' //If null, use a pre-determined image
               }
             ]
         });
@@ -22,8 +25,33 @@ module.exports = class Advice extends Command {
 
     async run(msg, { image }) {
       //Step 1, get image, turn into tags
+      image = await setImage(image);
+      const tags = await getTags(image);
+      console.log(tags);
     }
 
 
-
 };
+
+async function setImage(image){
+  return new Promise((resolve, reject) => {
+    if (image) {
+      resolve(image);
+    }
+    else {
+      fs.readdir(__dirname + '/srcImages', (err, items) => {
+        if (err) { throw err; }
+        image = __dirname + '/srcImages/' + items[Math.floor(Math.random() * items.length)]
+        resolve(image);
+      })
+    }
+  });
+}
+
+async function getTags(image) {
+  const client = new vision.ImageAnnotatorClient({
+    keyFilename: 'Not-A-Robot-de14d9a0dccc.json'
+  });
+  const labels = await client.labelDetection(image);
+  return labels;
+}
