4fb85b613f34c8587e88995de6181c913ec9d027
Tehmo3 <jasper.b.miles@gmail.com>
2018-05-09 12:40:59 +1000

    disguise generator now exists

--- commands/funcommands/disguise.js
+++ commands/funcommands/disguise.js
@@ -2,7 +2,6 @@ const { Command } = require('discord.js-commando');
 const request = require('request-promise-native');
 const vision = require('@google-cloud/vision');
 const fs = require('fs');
-// const path = require('path');
 
 module.exports = class Advice extends Command {
     constructor(client) {
@@ -25,9 +24,13 @@ module.exports = class Advice extends Command {
 
     async run(msg, { image }) {
       //Step 1, get image, turn into tags
+      msg.channel.
       image = await setImage(image);
       const tags = await getTags(image);
-      console.log(tags);
+      const name = await generateName(tags[0].labelAnnotations);
+      msg.channel.send(`Your new identity:\nName: ${name}`, {
+        file: image
+      });
     }
 
 
@@ -39,9 +42,9 @@ async function setImage(image){
       resolve(image);
     }
     else {
-      fs.readdir(__dirname + '/srcImages', (err, items) => {
+      fs.readdir(__dirname + '\\srcImages', (err, items) => {
         if (err) { throw err; }
-        image = __dirname + '/srcImages/' + items[Math.floor(Math.random() * items.length)]
+        image = __dirname + '\\srcImages\\' + items[Math.floor(Math.random() * items.length)]
         resolve(image);
       })
     }
@@ -54,4 +57,74 @@ async function getTags(image) {
   });
   const labels = await client.labelDetection(image);
   return labels;
+}
+
+async function generateName(tags) {
+  const option = Math.random();
+  tags = shuffle(tags);
+  if (option < 0.5) {
+    const word1 = await wordProcessingPipeline(tags[0].description);
+    const word2 = await wordProcessingPipeline(tags[1].description);
+    return `${word1} ${word2}`
+  }
+  else {
+    const word1 = await wordProcessingPipeline(tags[0].description);
+    const word2 = await wordProcessingPipeline(tags[1].description);
+    const word3 = await wordProcessingPipeline(tags[2].description);
+    return `${word1}, ${word2} and ${word3}`
+  }
+}
+
+async function wordProcessingPipeline(word) {
+  if (Math.random() < 0.5) {
+    return word;
+  }
+  if (Math.random() < 0.5) {
+    word = await rhyme(word);
+  }
+  if (Math.random() < 0.5) {
+    word = await soundLike(word);
+  }
+  if (Math.random() < 0.5) {
+    word = await synonym(word);
+  }
+  return word;
+}
+
+async function rhyme(word) {
+  let response = await request(`https://api.datamuse.com/words?rel_rhy=${word}&max=10`);
+  response = JSON.parse(response);
+  return response[Math.floor(Math.random() * response.length)].word;
+}
+
+async function soundLike(word) {
+  let response = await request(`https://api.datamuse.com/words?sl=${word}&max=10`);
+  response = JSON.parse(response);
+  return response[Math.floor(Math.random() * response.length)].word;
+}
+
+async function synonym(word) {
+  let response = await request(`https://api.datamuse.com/words?ml=${word}&max=10`);
+  response = JSON.parse(response);
+  return response[Math.floor(Math.random() * response.length)].word;
+}
+
+//https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
